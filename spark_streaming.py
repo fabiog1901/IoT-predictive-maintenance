651dac55f99a3d3ab79e3495d923b399d145afa5
fabiog <ghirardello.fabio@gmail.com>
2019-04-25 16:53:24 -0400

    Update spark_streaming.py

--- spark_streaming.py
+++ spark_streaming.py
@@ -48,7 +48,7 @@ def getPrediction(p):
               p['sensor_8'], p['sensor_9'], p['sensor_11'], p['sensor_12'],p['sensor_14'],p['sensor_15'],
               p['sensor_17'], p['sensor_18'], p['sensor_19'])
 
-    return = requests.post('http://YourCDSWDomain/api/altus-ds-1/models/call-model',
+    return requests.post('http://YourCDSWDomain/api/altus-ds-1/models/call-model',
                        data='{"accessKey":"YourAccessKey", "request":{"feature":"' + feature + '"}}',
                        headers={'Content-Type': 'application/json'}).json()['response']['result']
 
