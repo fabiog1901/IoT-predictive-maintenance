bc1bdccba9a561438fa9f23d0a3fc482bebec27d
fabiog <ghirardello.fabio@gmail.com>
2019-04-25 16:15:51 -0400

    Update iot_model.py

--- iot_model.py
+++ iot_model.py
@@ -1,7 +1,7 @@
 import pickle
 import numpy as np
 
-model = pickle.load(open('sklearn_rf.pkl', 'rb'))
+model = pickle.load(open('iot_model.pkl', 'rb'))
 
 def predict(args):
   account=np.array(args["feature"].split(",")).reshape(1,-1)
