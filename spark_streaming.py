04051a683b3d7e6db88ff8a0bdf9b8b7f25b426c
fabiog <ghirardello.fabio@gmail.com>
2019-04-26 12:48:24 -0400

    Update spark_streaming.py

--- spark_streaming.py
+++ spark_streaming.py
@@ -15,8 +15,10 @@ kudu_table = "impala::default.sensors"
 
 # define the table schema
 schema = StructType([StructField("sensor_id", IntegerType(), True),
-                     StructField("sensor_ts", IntegerType(), True),
+                     StructField("sensor_ts", LongType(), True),
+                     StructField("sensor_0", DoubleType(), True),
                      StructField("sensor_1", DoubleType(), True),
+                     StructField("sensor_2", DoubleType(), True),
                      StructField("sensor_3", DoubleType(), True),
                      StructField("sensor_4", DoubleType(), True),
                      StructField("sensor_5", DoubleType(), True),
@@ -26,14 +28,6 @@ schema = StructType([StructField("sensor_id", IntegerType(), True),
                      StructField("sensor_9", DoubleType(), True),
                      StructField("sensor_10", DoubleType(), True),
                      StructField("sensor_11", DoubleType(), True),
-                     StructField("sensor_12", DoubleType(), True),
-                     StructField("sensor_13", DoubleType(), True),
-                     StructField("sensor_14", DoubleType(), True),
-                     StructField("sensor_15", DoubleType(), True),
-                     StructField("sensor_16", DoubleType(), True),
-                     StructField("sensor_17", DoubleType(), True),
-                     StructField("sensor_18", DoubleType(), True),
-                     StructField("sensor_19", DoubleType(), True),
                      StructField("is_healthy", IntegerType(), True)])
 
 #Lazy SqlContext evaluation
@@ -44,11 +38,11 @@ def getSqlContextInstance(sparkContext):
 
 
 def getPrediction(p):
-    feature = "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s" % (p['sensor_4'], p['sensor_1'], p['sensor_6'],
-              p['sensor_8'], p['sensor_9'], p['sensor_11'], p['sensor_12'],p['sensor_14'],p['sensor_15'],
-              p['sensor_17'], p['sensor_18'], p['sensor_19'])
+    feature = "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s" % (p['sensor_1'], p['sensor_0'], p['sensor_2'],
+              p['sensor_3'], p['sensor_4'], p['sensor_5'], p['sensor_6'],p['sensor_7'],p['sensor_8'],
+              p['sensor_9'], p['sensor_10'], p['sensor_11'])
 
-    return requests.post('http://YourCDSWDomain/api/altus-ds-1/models/call-model',
+    return requests.post('http://cdsw.$PUBLIC_IP.nip.io/api/altus-ds-1/models/call-model',
                        data='{"accessKey":"YourAccessKey", "request":{"feature":"' + feature + '"}}',
                        headers={'Content-Type': 'application/json'}).json()['response']['result']
 
@@ -67,14 +61,16 @@ def insert_into_kudu(time,rdd):
 if __name__ == "__main__":
     sc = SparkContext(appName="SparkStreaming_IoT")
     ssc = StreamingContext(sc, 10) # 10 second window
-    kvs = KafkaUtils.createStream(ssc, zk_broker, "iot_ss", {kafka_topic:1})
+    kvs = KafkaUtils.createStream(ssc, zk_broker, "iot", {kafka_topic:1})
 
     # parse the kafka message into a tuple
     kafka_stream = kvs.map(lambda x: x[1]) \
                            .map(lambda l: json.loads(l)) \
                            .map(lambda p: (int(p['sensor_id']),
                                            int(p['sensor_ts']),
+                                           float(p['sensor_0']),
                                            float(p['sensor_1']),
+                                           float(p['sensor_2']),
                                            float(p['sensor_3']),
                                            float(p['sensor_4']),
                                            float(p['sensor_5']),
@@ -84,14 +80,6 @@ if __name__ == "__main__":
                                            float(p['sensor_9']),
                                            float(p['sensor_10']),
                                            float(p['sensor_11']),
-                                           float(p['sensor_12']),
-                                           float(p['sensor_13']),
-                                           float(p['sensor_14']),
-                                           float(p['sensor_15']),
-                                           float(p['sensor_16']),
-                                           float(p['sensor_17']),
-                                           float(p['sensor_18']),
-                                           float(p['sensor_19']),
                                            getPrediction(p)))
 
 
@@ -100,4 +88,3 @@ if __name__ == "__main__":
 
     ssc.start()
     ssc.awaitTermination()
-
